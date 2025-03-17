37c2627b25180f4661d5df8f8d6a70cd9f4e6459
Ruslan <ruslan.grudistov@gmail.com>
2025-03-17 17:08:32 +0200

    header search

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import sprite from "../../sprite.svg";
 import styles from "./Header.module.css";
 import FormHeader from "../Form-header/Form-header";
 
+
 const Header = ({ onFormSubmit }) => {
+  const [isActive, setIsActive] = useState(false);
+ 
+
+
   return (
     <header className={styles.header}>
       <nav>
@@ -13,7 +18,19 @@ const Header = ({ onFormSubmit }) => {
           </svg>
         </a>
       </nav>
+      <button onClick={()=>{
+        setIsActive(true)
+      }} className={`${styles.button} ${!isActive ? styles.active : `${styles.disactive} visually-hidden`}`}
+      >
+      <svg className={styles.search} width="16px" height="16px">
+          <use xlinkHref={`${sprite}#find`} />
+        </svg>
+      </button>
+      <div className={`${styles.button} ${!isActive ? `${styles.disactive} visually-hidden`  : styles.active}`}
+>
       <FormHeader hidden="hidden" handleFormSubmit={onFormSubmit} />
+      </div>
+      
       <ul className="hidden">
         <li>
           <a>Log in</a>
