5eae920a5b0105be228e1f8d38c0a535626318b0
Ruslan <ruslan.grudistov@gmail.com>
2025-12-05 11:38:00 +0200

    cross

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,19 +1,46 @@
 import styles from "./Input.module.css"
 
-export default function Input ({onChange, error, label, value, name, sup, placeholder}) {
+export default function Input ({onChange, error, label, value, name, sup, placeholder, data}) {
     const fieldError = error?.[name];
     console.log("error :", fieldError)
+
+    const clearIdiom = () => {
+        if (!onChange) return;
+
+        onChange({
+            target: {
+            name,
+            value: "",
+            },
+        });
+    };
 
     return(
         <label className={styles.label}>
             <span>{label} {sup && <sup className={styles.sup}>{sup}</sup>}</span>
-            <input
-                onChange={onChange}
-                className={`${styles.input} ${fieldError ? styles.inputError : ""}`}
-                value={value}
-                name={name}
-                placeholder={placeholder}
-            />
+            
+            <div className={styles.inputContainer}>
+                {value.length > 0 && (
+                    <button
+                        type="button"
+                        className={styles.clear}
+                        onClick={clearIdiom}
+                        aria-label="Clear search"
+                    >
+                        <svg className={styles.image} width="16" height="16" aria-hidden>
+                        <use xlinkHref="/sprite.svg#plus" />
+                        </svg>
+                    </button>
+                )}
+                <input
+                    onChange={onChange}
+                    className={`${styles.input} ${fieldError ? styles.inputError : ""}`}
+                    value={value}
+                    name={name}
+                    placeholder={placeholder}
+                />
+            </div>
+           
             {fieldError && <p className={styles.errorText}>{fieldError}</p>}
         </label>
     )
